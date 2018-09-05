5fb1681b2bca03ff23f4b64b3c07915af9adf846
Łukasz Solarski <lukasz.solar@gmail.com>
2018-09-06 00:06:28 +0200

    Spring MVC 2 - adding forgotten files, where Publishe and Book classes were upgraded for working with Thymelear

--- src/main/java/lukaszSolarski/springframework/spring5webapp/model/Publisher.java
+++ src/main/java/lukaszSolarski/springframework/spring5webapp/model/Publisher.java
@@ -13,10 +13,35 @@ public class Publisher {
     private String name;
     private String address;
 
+    public Publisher() {
+    }
+
     public Publisher(String name, String address) {
         this.name = name;
         this.address = address;
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
