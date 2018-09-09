025902ff3371b45fda081fe3f4837e47e61ca3f2
Łukasz Solarski <lukasz.solar@gmail.com>
2018-09-09 23:30:21 +0200

    Spring MVC 1 - create another controller and a view to show list of authors

---
+++ src/main/java/lukaszSolarski/springframework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,21 @@
+package lukaszSolarski.springframework.spring5webapp.controllers;
+
+import lukaszSolarski.springframework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+    public AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}
